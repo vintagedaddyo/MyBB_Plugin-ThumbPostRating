dee2b7556ec67dcb1b066666d48c2fb274034088
Vintagedaddyo <vintagedaddyo@gmail.com>
2019-08-12 18:59:10 -0700

    add italiano

---
+++ Upload/inc/languages/italiano/admin/thumbspostrating.lang.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * MyBB: Thumbs Post Rating
+ *
+ * File: thumbpostrating.php (admin)
+ *
+ * Authors: TY Yew & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ *  This file is part of Thumbs Post Rating plugin for MyBB.
+ *
+ *  Thumbs Post Rating plugin for MyBB is free software; you can
+ *  redistribute it and/or modify it under the terms of the GNU General
+ *  Public License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  Thumbs Post Rating plugin for MyBB is distributed in the hope that it
+ *  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http:www.gnu.org/licenses/>.
+ */
+
+//
+
+$l['tpr_info_name'] = 'Thumbs Post Rating';
+
+$l['tpr_info_desc'] = 'Aggiungi un sistema di valutazione pollice su / pollice giù su ogni singolo post.';
+
+//
+
+$l['tpr_set_group_1_title'] = 'Valutazione post pollici';
+
+$l['tpr_set_group_1_desc'] = 'Impostazioni per il plug-in di valutazione post Thumbs.';
+
+//
+
+$l['tpr_set_usergroups_title'] = 'Gruppi utenti';
+
+$l['tpr_set_usergroups_desc'] = 'Gruppi utenti che hanno permesso di valutare il post. (Impostazione predefinita: 2,3,4,6) <br /> 0 indica che tutti i gruppi di utenti possono valutare, tranne gli ospiti.';
+
+//
+
+$l['tpr_set_forums_title'] = 'Forum';
+
+$l['tpr_set_forums_desc'] = 'Forum da escludere per avere la funzione di valutazione post. 0 indica che tutti i forum possono utilizzare la funzione di valutazione. <br /> Nota: dovrai inserire anche l ID del forum secondario.';
+
+//
+
+$l['tpr_set_selfrate_title'] = 'Disabilita l autovalutazione';
+
+$l['tpr_set_selfrate_desc'] = 'Disabilitando l autovalutazione, l utente non può valutare il proprio post.';
+
+//
+
+$l['tpr_set_undorate_title'] = 'Abilita annullamento della valutazione';
+
+$l['tpr_set_undorate_desc'] = 'Se abilitato, l utente può rimuovere (e modificare) il proprio voto dopo aver valutato un post.';
+
+//
+
+$l['tpr_set_closerate_title'] = 'Abilita valutazione a thread chiuso';
+
+$l['tpr_set_closerate_desc'] = 'Se abilitato, l utente può valutare i post in un thread chiuso.';
+
+?>
---
+++ Upload/inc/languages/italiano/thumbspostrating.lang.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * MyBB: Thumbs Post Rating
+ *
+ * File: thumbpostrating.lang.php
+ *
+ * Authors: TY Yew & Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.4
+ *
+ *  This file is part of Thumbs Post Rating plugin for MyBB.
+ *
+ *  Thumbs Post Rating plugin for MyBB is free software; you can
+ *  redistribute it and/or modify it under the terms of the GNU General
+ *  Public License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  Thumbs Post Rating plugin for MyBB is distributed in the hope that it
+ *  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http:www.gnu.org/licenses/>.
+ */
+
+//
+
+$l['tpr_rate_up'] = 'Vota il pollice in alto';
+
+//
+
+$l['tpr_rate_down'] = 'Giudica i pollici giu';
+
+//
+
+$l['tpr_undo_rate'] = 'Annulla valutazione';
+
+//
+
+$l['tpr_error_invalid_rating'] = 'Input di valutazione non valido.';
+
+//
+
+$l['tpr_error_cannot_rate'] = 'Non puoi votare questo post.';
+
+//
+
+$l['tpr_error_already_rated'] = 'Hai gia valutato questo post.';
+
+?>
